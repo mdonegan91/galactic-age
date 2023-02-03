7ec8fd2a63f2ae83546100014ca829473b61b6a6
Molly Donegan <mollyrdonegan@gmail.com>
2023-02-03 10:24:34 -0800

    good fail mercuryAge test

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -1,26 +1,25 @@
 import { Galactic } from './../src/js/galactic-age.js';
-// import { haikuGenerator } from './../src/js/haiku-generator.js';
 
 describe('Galactic', () => {
   let galactic;
-  beforeEach(() => { 
+  beforeEach(() => {
     galactic = new Galactic(31, 21, 100);
   });
 
-  test('should create galactic object with user input age and life expectancy parameters', () => {
-    expect(galactic).toEqual({age: 31, past: 21, future: 100});
+  test('should create an object with user current age, past age and future age parameters', () => {
+    expect(galactic).toEqual({ age: 31, past: 21, future: 100 });
   });
 });
 
-// describe('Galactic', () => {
-//   let galactic;
-//   beforeEach(() => { 
-//     galactic = new Galactic(31, 21, 100);
-//     galactic.calcAge();
-//   });
+describe('Galactic', () => {
+  let galactic;
+  beforeEach(() => {
+    galactic = new Galactic(31, 21, 100);
+    galactic.getAge();
+  });
 
-//   test('should create Galactic method that returns user inputted age in Mercury years.', () => {
-//     expect(galactic.mercuryAge).toEqual("103.69");
-//   });
-// });
+  test('should create a method that returns user inputted age in Mercury years.', () => {
+    expect(galactic.mercuryAge).toEqual("128.58");
+  });
+});
 
