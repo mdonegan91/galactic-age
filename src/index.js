e2f36a7bf56ce92c31228a6662d1856c7b36e8fa
Molly Donegan <mollyrdonegan@gmail.com>
2023-02-03 16:53:44 -0800

    fix UI bugs

--- src/index.js
+++ src/index.js
@@ -3,23 +3,52 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { Galactic } from './js/galactic-age.js';
 
-function blastOff(e) {
-  e.preventDefault();
+function blastOff(event) {
+  event.preventDefault();
   const newAge = document.getElementById("age").value;
   const newPast = document.getElementById("past").value;
   const newFuture = document.getElementById("future").value;
   const planet = document.querySelector("input[name='planet']:checked").value;
-  const paragraph = document.createElement("p");
-  const h2 = document.createElement("h2");
-
   let galactic = new Galactic(newAge, newPast, newFuture);
+  galactic.getAge();
+  galactic.getPast();
+  galactic.getFuture();
 
-  h2.append("Blasting Off!");
-  paragraph.append(`Your age on ${planet} is ${galactic.getAge(planet)} years. ${planet} years since your favorite age: ${galactic.getPast(planet)}. Years until your favorite age: ${galactic.getFuture(planet)}`);
+  if (planet === "mercury") {
 
-  document.body.append(h2, paragraph);
+  document.querySelector("div#mercury-trip").removeAttribute("class", "hidden");
+  document.getElementById("mercury-age").innerText = galactic.mercuryAge;
+  document.getElementById("mercury-past-age").innerText = galactic.mercuryPast;
+  document.getElementById("mercury-future-age").innerText = galactic.mercuryFuture;
+  } else if (planet === "venus") {
+
+  document.querySelector("div#venus-trip").removeAttribute("class", "hidden");
+  document.getElementById("venus-age").innerText = galactic.venusAge;
+  document.getElementById("venus-past-age").innerText = galactic.venusPast;
+  document.getElementById("venus-future-age").innerText = galactic.venusFuture;
+  } else if (planet === "mars") {
+
+  document.querySelector("div#mars-trip").removeAttribute("class", "hidden");
+  document.getElementById("mars-age").innerText = galactic.marsAge;
+  document.getElementById("mars-past-age").innerText = galactic.marsPast;
+  document.getElementById("mars-future-age").innerText = galactic.marsFuture;
+  } else if (planet === "jupiter") {
+
+  document.querySelector("div#jupiter-trip").removeAttribute("class", "hidden");
+  document.getElementById("jupiter-age").innerText = galactic.jupiterAge;
+  document.getElementById("jupiter-past-age").innerText = galactic.jupiterPast;
+  document.getElementById("jupiter-future-age").innerText = galactic.jupiterFuture;
+}
+}
+
+function backToEarth() {
+  document.querySelector("button.reset").removeAttribute("id");
+  document.querySelector("div#results").setAttribute("class", "hidden");
+  document.getElementById("submit").disabled = false;
+  document.getElementById("reset").disabled = false;
 }
 
 window.addEventListener("load", function () {
   document.querySelector("form#age").addEventListener("submit", blastOff);
+  document.querySelector("button.reset").addEventListener("click", backToEarth);
 });
